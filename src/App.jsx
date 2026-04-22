342cba468daebab9c64299f85712ab5a15948faa
nandkishorasambe <nandkishorasambe98@gmail.com>
2026-04-22 15:31:36 +0530

    add new code

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+       
+        <div>
+          <h1>Get started</h1>
+          <p>
+           nandkishor
+          </p>
+        </div>
+       
+      </section>
+
+          </>
+  )
+}
+
+export default App
